3dd406591596de3469fbda7c7cf4540b9001b22a
Harishankar Sugumaran <harishankar.sugumaran@gmail.com>
2023-04-04 09:03:25 +1000

    display csv data in web app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
@@ -9,3 +10,11 @@ streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+# load the data from csv file to a pandas dataframe
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# display the data from csv file in streamlit web app
+streamlit.dataframe(my_fruit_list)
+
+# app url : https://harishankarsugumaran-first-streamlit-app-streamlit-app-u3y3yt.streamlit.app/
