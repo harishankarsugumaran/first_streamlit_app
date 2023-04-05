d0c8d4f321ccf25181784e0eabfffc3c6079235e
Harishankar Sugumaran <harishankar.sugumaran@gmail.com>
2023-04-05 21:47:48 +1000

    removed text

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,6 @@ except URLError as e:
     streamlit.error()
 
 # Display trial account metadata
-streamlit.text("The fruit load list contains:")
 # Snowflake related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
